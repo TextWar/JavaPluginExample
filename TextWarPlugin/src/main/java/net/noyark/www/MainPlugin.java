fdcd350b3d489964289d0e1c00894a5f204d6c74
magiclu550 <843983728@qq.com>
2020-04-03 15:47:16 +0800

    example

---
+++ TextWarPlugin/src/main/java/net/noyark/www/MainPlugin.java
@@ -0,0 +1,23 @@
+package net.noyark.www;
+
+import cn.textwar.plugins.EnableRegister;
+import cn.textwar.plugins.Main;
+import cn.textwar.plugins.PluginBase;
+
+@Main(name = "example")
+@EnableRegister
+public class MainPlugin extends PluginBase {
+
+    public void onLoad() {
+
+    }
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
---
+++ TextWarPlugin/src/main/java/net/noyark/www/PlayerListener.java
@@ -0,0 +1,13 @@
+package net.noyark.www;
+
+import cn.textwar.plugins.EventManager;
+import cn.textwar.plugins.Listener;
+import cn.textwar.plugins.events.PlayerJoinEvent;
+
+public class PlayerListener implements Listener {
+
+    @EventManager
+    public void onPlayerJoin(PlayerJoinEvent e){
+
+    }
+}
